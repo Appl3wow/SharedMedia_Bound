fd3dee98f99b94301215aa88c99cb917824ea55b
Apple Man <appl3man@live.com>
2023-12-31 23:38:31 -0800

    initial commit

---
+++ Soundpaths.lua
@@ -0,0 +1,24 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cff00ff00That's His Cock!|r", [[Interface\Addons\SharedMedia_Bound\sound\ThatsHisCock.mp3]])
+LSM:Register("sound", "|cff00ff00Orgim Ahhhh|r", [[Interface\Addons\SharedMedia_Bound\sound\ahhh.mp3]])
+
+-- -----
+--   STATUSBAR
+-- -----
+`
