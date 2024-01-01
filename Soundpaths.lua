349ee0f348c2b89f40db33fdc4de8dc03434b29a
Apple Man <appl3man@live.com>
2024-01-01 01:18:49 -0800

    remove innapropriate sound

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -15,7 +15,7 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 --   SOUND
 -- -----
-LSM:Register("sound", "|cff00ff00That's His Cock!|r", [[Interface\Addons\SharedMedia_Bound\sound\ThatsHisCock.mp3]])
+LSM:Register("sound", "|cff00ff00Eggplant|r", [[Interface\Addons\SharedMedia_Bound\sound\eggplant.mp3]])
 LSM:Register("sound", "|cff00ff00Orgim Ahhhh|r", [[Interface\Addons\SharedMedia_Bound\sound\ahhh.mp3]])
 
 -- -----
